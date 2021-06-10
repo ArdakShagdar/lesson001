3dd452ea25810fd2a209fffccbfdf9bfef0ed7b1
Ardak Shagdar <ardakshagdar@gmail.com>
2021-06-10 12:00:32 +0600

    Первый коммит

---
+++ scritp.js
@@ -0,0 +1,25 @@
+alert('Hello World!');
+{
+    var $money = 7855961;
+}
+console.log('$money: ', $money);
+{
+    var myIncome = 999999;
+}
+console.log('myIncome: ', myIncome);
+{
+    var myAddExpenses = 88888888;
+}
+console.log('myAddExpenses: ', myAddExpenses);
+{
+    var myDeposit = 100000000;
+}
+console.log('myDeposit: ', myDeposit);
+{
+    var myMission = 999999999999;
+}
+console.log('myMission: ', myMission);
+{
+    var myPeriod = 365;
+}
+console.log('myPeriod: ', myPeriod);
